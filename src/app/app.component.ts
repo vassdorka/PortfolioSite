ffebe5ff29617d0c78ef0633c9508c12eadabef5
vassdorka <vassdorottya022@gmail.com>
2024-03-20 12:22:22 +0100

    building website content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio-app';
+
+  ngAfterViewInit(): void {
+    let worksContainer = document.querySelector('.works');
+    let scrollToGif = document.querySelector('.algomem-4');
+    let scrollBackGif = document.querySelector('.algomem-3');
+    scrollToGif?.addEventListener("mouseenter", (e) => {
+      worksContainer?.scrollTo({
+        top: worksContainer?.clientHeight,
+        left: 800,
+        behavior: "smooth",
+      })
+    });
+    scrollBackGif?.addEventListener("mouseenter", (e) => {
+      worksContainer?.scrollTo({
+        top: worksContainer?.clientHeight,
+        left: 0,
+        behavior: "smooth",
+      })
+    });
+  }
 }
